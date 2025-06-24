5db1e6abec40a9754dd2ff8e6aa90d7df2451e47
James Ma <jamesma2324@berkeley.edu>
2025-06-23 23:55:16 -0700

    ere

--- app.js
+++ app.js
@@ -22,10 +22,7 @@ app.get('/', (req, res) => {
 });
 
 module.exports = app;
-const PORT = process.env.PORT || 5050;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
-});
+
 
 
 
