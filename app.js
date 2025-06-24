0777ec322718d80f1b9573716376bd5477d88173
James Ma <jamesma2324@berkeley.edu>
2025-06-23 23:46:52 -0700

    wewe

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.get('/', (req, res) => {
 
 module.exports = app;
 const PORT = process.env.PORT || 5050;
-app.listen(PORT, () => {
-    console.log(`✅ Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
 });
 
 
